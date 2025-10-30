e449f4e0a9624f7a7b0a51e9dcbe68b48dc996a7
Mark <knotzermark@gmail.com>
2025-10-30 17:43:25 +0100

    rename HelloController and HelloApplication to LoginController and CampusConnectApplication and add login ui with controller boilerplate code

---
+++ src/main/java/at/ac/hcw/campusconnect/CampusConnectApplication.java
@@ -0,0 +1,29 @@
+package at.ac.hcw.campusconnect;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class CampusConnectApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(CampusConnectApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Image logo = new Image(
+                Objects.requireNonNull(getClass().getResourceAsStream("images/logo.png"))
+        );
+        stage.getIcons().add(logo);
+        stage.setTitle("CampusConnect");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/at/ac/hcw/campusconnect/LoginController.java
@@ -0,0 +1,14 @@
+package at.ac.hcw.campusconnect;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+    public void handleSendCode(ActionEvent actionEvent) {
+    }
+
+    public void handleVerifyCode(ActionEvent actionEvent) {
+    }
+}
