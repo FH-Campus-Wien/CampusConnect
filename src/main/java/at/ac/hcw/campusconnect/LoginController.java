dad943b86acae1ca142e76f95ad507ae72a4a229
Mark <knotzermark@gmail.com>
2025-10-30 18:28:01 +0100

    add logo image via controller

--- src/main/java/at/ac/hcw/campusconnect/LoginController.java
+++ src/main/java/at/ac/hcw/campusconnect/LoginController.java
@@ -3,8 +3,21 @@ package at.ac.hcw.campusconnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
 
 public class LoginController {
+    @FXML
+    private ImageView logoImageView;
+
+    @FXML
+    public void initialize() {
+        Image logo = new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/logo.png")));
+        logoImageView.setImage(logo);
+    }
+
 
     public void handleSendCode(ActionEvent actionEvent) {
     }
